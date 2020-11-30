4d5a7b5702a35a8c90de2337ef1459e7aab8eb35
kubabasiakowski <kubabasiakowski@users.noreply.github.com>
2020-11-30 11:24:52 +0100

    3rd commit

--- app/code/MagentoTask/WeatherModule/Block/index.php
+++ app/code/MagentoTask/WeatherModule/Block/index.php
@@ -15,20 +15,6 @@ class Index extends \Magento\Framework\View\Element\Template {
 		parent::__construct($context);
     }
 
-	public function currentWeather(){
-		$api_key = "7728d767a3017544ca1728e0798e94d6";
-		$city_id = "765876";
-		$api_url = "http://api.openweathermap.org/data/2.5/weather?id=".$city_id."&appid=".$api_key;
-
-		$json=file_get_contents($api_url);
-		$data=json_decode($json,true);
-
-
-        echo "<h2>Current Temperature in Lublin is " . floatval($data['main']['temp']) . " stopni Kelvina (" . floatval($data['main']['temp'] - 272.15) . " stopni Celsjusza)";
-
-
-	}
-
 	public function getWeatherItems()
     {
         $collectionItems = $this->getWeatherCollection();
